a6dc22c6d3df1c1b949679acf67763054211597c
ZebaSulthana <52819185+ZebaSulthana@users.noreply.github.com>
2019-07-12 15:41:52 +0530

    Create pl5.py

---
+++ pl5.py
@@ -0,0 +1,5 @@
+alp=input()
+k=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVII','XIX','XX']
+for i in range(0,len(k)):
+    if alp==k[i]:
+        print(i+1)
