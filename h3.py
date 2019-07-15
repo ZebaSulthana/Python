b8ad3fc2a49e0b1c82bb6227c2020bfb4699e4ed
ZebaSulthana <52819185+ZebaSulthana@users.noreply.github.com>
2019-07-15 15:26:33 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,7 +1,12 @@
 alp=int(input())
 h=input()
+flag=0
 s=list(map(int,h.split()))
 if len(s)==alp:
     for i in range(0,alp):
         if s[i]==i:
+            flag=1
             print(s[i],end=" ")
+if flag!=1:    
+    print("-1")
+    
