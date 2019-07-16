8db4c59823de67eeb334d359eb2521e61bc623c3
ZebaSulthana <52819185+ZebaSulthana@users.noreply.github.com>
2019-07-16 16:03:21 +0530

    Create pro17.py

---
+++ pro17.py
@@ -0,0 +1,20 @@
+alp=input()
+
+a=list(map(int,alp.split()))
+
+k=a[1]
+h=input()
+
+s=list(map(int,h.split()))
+flag=0
+for i in range(0,len(s)-1):
+	for j in range(i+1,len(s)):
+		if s[i]+s[j]==k:
+			print("yes")
+			flag=1
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")
+		
